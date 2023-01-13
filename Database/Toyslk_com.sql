84ac94d386c1fabb8d961dbee74f2f72f81621ea
Seyon Suriyakumaran <83668877+SeyonSuriya@users.noreply.github.com>
2023-01-13 19:23:20 +0530

    Update Toyslk_com.sql

--- Database/Toyslk_com.sql
+++ Database/Toyslk_com.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 11, 2023 at 10:42 AM
+-- Generation Time: Jan 14, 2023 at 03:20 AM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -38,34 +38,35 @@ CREATE TABLE `cart` (
 --
 
 INSERT INTO `cart` (`customerID`, `productID`, `units`) VALUES
-(1, 1, 62),
-(1, 2, 13),
-(3, 1, 5);
+(3, 1, 5),
+(2, 1, 10);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Customer`
+-- Table structure for table `customers`
 --
 
-CREATE TABLE `Customer` (
-  `customerID` int(5) NOT NULL,
-  `customerName` varchar(30) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
-  `addressLine1` varchar(50) NOT NULL,
-  `addressLine2` varchar(50) NOT NULL,
-  `District` varchar(15) NOT NULL,
-  `Province` varchar(15) NOT NULL,
-  `Country` varchar(15) NOT NULL,
-  `postalCode` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `customers` (
+  `customerID` int(6) NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `email` text NOT NULL,
+  `addressline1` text NOT NULL,
+  `addressline2` text,
+  `addressline3` text,
+  `phonenumber` text NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Customer`
+-- Dumping data for table `customers`
 --
 
-INSERT INTO `Customer` (`customerID`, `customerName`, `mobile`, `addressLine1`, `addressLine2`, `District`, `Province`, `Country`, `postalCode`) VALUES
-(2, 'Hiran Sanjeewa', '0766475117', 'No 40, wewa kotasa, puhulwatta', 'maussagolla, passara', 'Badulla', 'Uva', 'Srilanka', 90500);
+INSERT INTO `customers` (`customerID`, `firstname`, `lastname`, `email`, `addressline1`, `addressline2`, `addressline3`, `phonenumber`, `password`) VALUES
+(1, 'saman', 'kumara', 'abc@gmail.com', 'sa', 'kj', 'ya', '0766199583', '123456'),
+(2, 'kasun', 'kalhara', 'abc3@gmail.com', 'saa', 'jjkk', 'ytrr', '076615589', '123789'),
+(3, 'Hiran Sanjeewa', 'Basnayaka', 'horansanjeewaa@gmail.com', 'no 40 , wewa kotasa, puhulwatta', 'maussagolla,passara', '', '0776605987', 'Hiran12345');
 
 -- --------------------------------------------------------
 
@@ -81,21 +82,29 @@ CREATE TABLE `product` (
   `price` varchar(5) NOT NULL,
   `weight` varchar(4) NOT NULL,
   `availableUnits` int(5) NOT NULL,
-  `numberOfOrders` int(4) DEFAULT NULL
+  `numberOfOrders` int(4) DEFAULT NULL,
+  `category` enum('other','Cars and vehical','Construction toys','Dolls','Educational toys','Electronic toys','Food-related toys','Puzzle/assembly','Science and optical','Sound toys','Spinning toys','Wooden toys') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`productID`, `name`, `secondaryName`, `description`, `price`, `weight`, `availableUnits`, `numberOfOrders`) VALUES
-(1, 'Kids Jeep', 'Kids Jeep/ Car/ Vehicle', 'Baybee Magento Rechargeable Battery Operated Jeep for Kids, Ride on Toy Kids Car with Bluetooth & Music| Baby Big Battery Car | Electric Jeep Car for Kids to Drive 3 to 8 Years Boys Girls (Blue II)\r\n', '5000', '0.5', 100, 10),
-(2, 'Flying Helicopter', 'Remote Control Mini Drone Flying Helicopter', 'Remote Control Mini Drone Flying Helicopter Infraed Induction Kid Toys Aircraft LED Drone Flying Suspension Induction Helicopter', '16155', '0.25', 234, 56),
-(3, 'Alloy Finger Bicycle', 'Bicycle Diecast Metal Bike Model Racing Toy', 'Alloy Finger Bicycle Diecast Metal Bike Model Racing Toy Simulation Collection Toys for Children Adult Mini Desk Funny Bike', '500', '0.2', 1, 34);
+INSERT INTO `product` (`productID`, `name`, `secondaryName`, `description`, `price`, `weight`, `availableUnits`, `numberOfOrders`, `category`) VALUES
+(1, 'Kids Jeep', 'Kids Jeep/ Car/ Vehicle', 'Baybee Magento Rechargeable Battery Operated Jeep for Kids, Ride on Toy Kids Car with Bluetooth & Music| Baby Big Battery Car | Electric Jeep Car for Kids to Drive 3 to 8 Years Boys Girls (Blue II)\r\n', '400', '0.5', 150, 199, 'other'),
+(2, 'Flying Helicopter', 'Remote Control Mini Drone Flying Helicopter', 'Remote Control Mini Drone Flying Helicopter Infraed Induction Kid Toys Aircraft LED Drone Flying Suspension Induction Helicopter', '300', '0.25', 135, 286, 'other'),
+(3, 'Alloy Finger Bicycle', 'Bicycle Diecast Metal Bike Model Racing Toy', 'Alloy Finger Bicycle Diecast Metal Bike Model Racing Toy Simulation Collection Toys for Children Adult Mini Desk Funny Bike', '200', '0.2', 98, 307, 'other'),
+(4, 'sfdfsd', 'sadasdasd', 'asdasdasd', '2313', '1', 12, 21, 'Cars and vehical');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customerID`);
 
 --
 -- Indexes for table `product`
@@ -108,10 +117,16 @@ ALTER TABLE `product`
 --
 
 --
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customerID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `productID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `productID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
